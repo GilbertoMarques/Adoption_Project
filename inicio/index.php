28b142ef438db1bdb6b7dca1a43d96b67e1d1a71
Gilberto Marques <gilberto.oliveira11@gmail.com>
2022-06-05 02:44:49 -0300

    commit inicial

--- inicio/index.php
+++ inicio/index.php
@@ -3,89 +3,103 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="icon" type="imagem/png" href="logo.png"/>
     <title>Início | Focinho Feliz</title>
+    <!-- ==== CSS ==== -->
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+    <!-- NavBar -->
+    <nav class="navbar">
+    <img src="logo2.png" alt="logo2.png" style="width:120px;height:60px;object-fit:cover;object-position:50% 50%">
+        <ul class="nav-links">
+            <li class="btn"><a href="#">Início</a></li>
+            <li class="btn"><a href="#events">Eventos</a></li>
+            <li class="btn"><a href="#adopt">Adotar</a></li>
+            <li class="btn"><a href="#about">Sobre</a></li>
+            <li class="btn"><a href="#contact">Contato</a></li>
+            <li class="btn"><a href="../login/logout.php">Sair</a></li>
+        </ul>
+        <img src="menu-btn.png" alt="" class="menu-btn">
+    </nav>
 
     <header>
-        <div class="container">
-
-            <div class="logo">
-                <img src="logo1.png" alt="logo1.png" style="width:120px;height:60px;object-fit:cover;object-position:50% 50%">
-            </div>
-
-            <div class="menu-section">
-
-                <div class="menu-burguer">
-                    <div class="one"></div>
-                    <div class="two"></div>
-                    <div class="three"></div>
-                </div>
-
-                <nav>
-                    <ul>
-                        <li><a href="#">Início</a></li>
-                        <li><a href="#">Sobre</a></li>
-                        <li><a href="#">Adotar</a></li>
-                        <li><a href="#">Contato</a></li>
-                        <li><a href="../login/logout.php">Sair</a></li>
-                    </ul>
-                </nav>
-
-            </div>
-        
+        <div class="header-content">
+            <h2>Adoção Responsável</h2>
+            <div class="line"></div>
+            <p>Conheça os animais da Focinho Feliz - DF,<br>que estão disponíveis para adoção. </p>
+            <a href="https://forms.gle/L8GWoTrkXQq4eU6UA" class="btn">Quero adotar</a>
         </div>
-        
     </header>
-  
-        <div class="slider">
 
-            <div class="slides">
-                
-                <input type="radio" name="radio-btn" id="radio1">
-                <input type="radio" name="radio-btn" id="radio2">
-                <input type="radio" name="radio-btn" id="radio3">
-                <input type="radio" name="radio-btn" id="radio4">
-
-                <div class="slide first">
-                    <img src="bgn1.png" alt="">
-                </div>
-
-                <div class="slide">
-                    <img src="bgn3.png" alt="">
-                </div>
-
-                <div class="slide">
-                    <img src="bgn2.png" alt="">
-                </div>
-
-                <div class="slide">
-                    <img src="bgn.png" alt="">
-                </div>
-
-                <div class="navigation-auto">
-                    <div class="auto-btn1"></div>
-                    <div class="auto-btn2"></div>
-                    <div class="auto-btn3"></div>
-                    <div class="auto-btn4"></div>
-                </div>
-
-            </div>
-            
-              <div class="navigation-bar">
-                <label class="manual-btn" for="radio1"></label>
-                <label class="manual-btn" for="radio2"></label>
-                <label class="manual-btn" for="radio3"></label>
-                <label class="manual-btn" for="radio4"></label>
-              </div>
-
-            </div>
-
+    <!-- == Events == -->
+    <section class="events" id="events">
+        <div class="title">
+            <h1>EVENTOS DE ADOÇÃO</h1>
+            <div class="line"></div>
         </div>
+        <div class="row">
+            <div class="col">
+                <img src="bg4.jpg" alt="">
+                <h4>1° Evento de Adoção Focinho Feliz DF 2022</h4>
+                <p>Data: 11/06 | Horário: 09h às 13h | Local: Castelinho do Parque da Cidade - Brasília – Srps - Asa Sul (ao lado do Castelinho).</p>
+                <a href="#" class="btn">Saber mais</a>
+            </div>
+            <div class="col">
+                <img src="bg5.jpg" alt="">
+                <h4>1° Feira de Adoção Focinho Feliz DF 2022</h4>
+                <p>Data: 08/07 | Horário: 09h às 13h | Local: Lago do Parque da Cidade - Brasília – Srps - Asa Sul (ao lado do Pedalinho).</p>
+                <a href="#" class="btn">Saber mais</a>
+            </div>
+        </div>
+    </section>
 
-    <script src="menu.js"></script>
+    <section class="adopt" id="adopt">
+        <div class="adopt-content">     
+            <h1>Adote</h1>
+            <div class="line"></div>
+            <p>Esse momento é essencial antes de procurar um cachorro para doação. 
+            Sendo assim, para começar faça uma análise das vantagens e desvantagens 
+            ao adotar antes de tomar a decisão.
+            Sabia que um cão vive entre 10 a 13 anos? Ele fará parte da família durante 
+            um longo tempo. Por isso, lembre-se que os outros moradores da casa necessitam 
+            concordar com a presença do amigo no ambiente.</p>
+            <a href="https://forms.gle/L8GWoTrkXQq4eU6UA" class="btn">Quero adotar</a>
+        </div>
+    </section>
 
+    <section class="about" id="about">
+        <div class="row">
+            <div class="col content-col">
+                <h1>Sobre</h1>
+                <div class="line"></div>
+                <p>Criado em 2018, o abrigo miau resgata, trata e encaminha
+                para a adoção cães e gatos em situação de rua e maus tratos
+                em Brasília/DF. Já ajudaram mais de 300 animais a encontrar
+                um novo lar, acreditam que a solidariedade gerada pela
+                ternura dos animais movem eles. Os cuidados da
+                Focinho Feliz se extendem aos animais que ainda não têm
+                uma família e adoção é um assunto sério por aqui. Aprenda sobre guarda
+                responsável, como resgatar um animal e tudo sobre esse universo.</p>
+                <a href="#" class="btn">Perguntas frequentes</a>
+            </div>
+            <div class="col image-col">
+                <div class="image-gallery">
+                    <img src="bg.jpeg" alt="">
+                    <img src="bg6.jpg" alt="">
+                    <img src="bg1.jpg" alt="">
+                    <img src="bg7.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="footer" id="contact">
+        <p>Telefones: Kelly (61) 98260-2887 | Lívia (61) 98212-2949<br>
+​           Endereço: Jardim Botânico, Brasília - DF, CEP 71680601<br>
+           Horário de Funcionamento: 9h às 17h (nos dias úteis)<br>
+​           Horário para Adoção: 10h as 16h (aos sábados) COBASI 516 Norte</p>
+        <p>Copyright © 2022 Focinho Feliz</p>
+    </section>
 </body>
 </html>
